fa91dc533d5694bec3f688ac50d5ecb05a1ce578
Ethanm-0371 <ethanm0371@gmail.com>
2023-04-18 01:54:00 +0200

    Populated map. - Filled with generic NPCs and placed specfic ones in their places.

--- Output/Assets/Maps/NPCs.tsx
+++ Output/Assets/Maps/NPCs.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="NPCs" tilewidth="16" tileheight="32" tilecount="3" columns="3">
- <image source="../Textures/NPCs/NPCs.png" width="48" height="32"/>
+<tileset version="1.10" tiledversion="1.10.0" name="NPCs" tilewidth="16" tileheight="32" tilecount="4" columns="4">
+ <image source="../Textures/NPCs/NPCs.png" width="64" height="32"/>
  <tile id="0">
   <properties>
    <property name="isWalkable" type="bool" value="false"/>
@@ -12,6 +12,11 @@
   </properties>
  </tile>
  <tile id="2">
+  <properties>
+   <property name="isWalkable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="3">
   <properties>
    <property name="isWalkable" type="bool" value="false"/>
   </properties>
