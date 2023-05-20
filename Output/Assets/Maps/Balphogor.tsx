03ad34e15a62d5ddf1d759bffd7a5f8d306ce358
Uri <oriol253@gmail.com>
2023-05-20 08:15:19 +0200

    Cleaned up unused assets

--- Output/Assets/Maps/Balphogor.tsx
+++
@@ -1,6 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="Balphogor" tilewidth="48" tileheight="96" tilecount="9" columns="1" objectalignment="topleft">
- <tileoffset x="0" y="-48"/>
- <grid orientation="orthogonal" width="16" height="32"/>
- <image source="../Textures/NPCs/Balphogor.png" width="48" height="96"/>
-</tileset>
